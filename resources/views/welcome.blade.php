61cf307aef303d9ddf9fb98ee101929de8e305fd
Omar Villafuerte <ovillafuerte.94@gmail.com>
2021-04-13 06:17:24 -0400

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,9 @@
     <h4>Listado de etiquetas</h4>
     <table>
         @forelse ($tags as $tag)
-
+        <tr>
+            <td>{{ $tag->name }}</td>
+        </tr>
         @empty
         <tr>
             <td>
